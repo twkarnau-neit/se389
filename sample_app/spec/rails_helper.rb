9d3eb13463b72a1f1ac902aedb5fb03c08991b9d
Thomas Karnau <twkarnau@email.neit.edu>
2014-08-26 17:43:57 -0700

    Fixing rspec test gems for running tests

--- sample_app/spec/rails_helper.rb
+++ sample_app/spec/rails_helper.rb
@@ -1,6 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-require 'spec_helper'
+#require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
@@ -40,5 +40,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  config.order = "random"
   config.include Capybara::DSL
+
 end
